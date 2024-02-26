42dea6189e5e7176fd756695a4dfe1aec6a81b7e
Charlie <124cscott@frhsd.com>
2024-02-26 10:18:27 -0500

    Lesson 5

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'https://www.vuemastery.com/',
              inventory:100,
-             onSale: false
+             onSale: false,
+             details: ['50% cotton', '30% wool', '20% polyster'],
+             variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue'},
+             ],
+             sizes: ['xsmall', 'small', 'medium', 'large', 'xlarge',],
         }
     }
 })
