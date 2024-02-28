412dc1078ece87c6e9aa6001d05898d9de51dbbe
Charlie <124cscott@frhsd.com>
2024-02-28 08:17:16 -0500

    Lesson 7

--- main.js
+++ main.js
@@ -6,17 +6,20 @@ const app = Vue.createApp({
             description: 'They are socks',
             image: './assets/images/socks_green.jpg',
             url: 'https://www.vuemastery.com/',
-             inventory:100,
+            inventory: 8,
              onSale: false,
              details: ['50% cotton', '30% wool', '20% polyster'],
              variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
              ],
              sizes: ['xsmall', 'small', 'medium', 'large', 'xlarge'],
 
         }
     },
+
+
+
 methods:
  {
     addToCart() {
@@ -24,9 +27,18 @@ methods:
     },
     removeFromCart() {
         if(this.cart >= 0)
-        this.cart-= 1;
+        this.cart -= 1;
     },
     updateImage(variantImage) {
         this.image = variantImage;
-    }
- }})
+    },
+    computed: {
+    title() {
+        return this.brand + ' ' + this.product
+    },
+    inStock() {
+        return this.inventory > 0;
+    },
+}
+ }
+})
