4d9cc0b7f2139aa0a445e4f862ed0cebcc801f17
Charlie <124cscott@frhsd.com>
2024-02-29 08:56:30 -0500

    Fixed Lesson 8 (Kinda a Reset)

--- main.js
+++ main.js
@@ -1,54 +1,49 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            product: 'Socks',
-            brand: 'VueMastery',
-            description: 'They are socks',
-           selectedVariant: 0,
-            url: 'https://www.vuemastery.com/',
-            inventory: 8,
-             onSale: true,
-             details: ['50% cotton', '30% wool', '20% polyster'],
-             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0},
-             ],
-             sizes: ['xsmall', 'small', 'medium', 'large', 'xlarge'],
-
+    data () {
+        return{
+            cart:0,
+            brand: "Vue Mastery",
+            product: "Socks",
+            description: "They are socks",
+            selectedVariant: 0,
+            url: "https://www.vuemastery.com",
+            inventory: 0,
+            onSale: true,
+            details: ["50 cotton", "30% wool", "20% polyester"],
+            variants:[
+                { id: 2234, color: 'green', image: "./assets/images/socks_green.jpg", quantity: 50},
+                { id: 2235, color: 'blue', image: "./assets/images/socks_blue.jpg", quantity: 0}
+            ],
+            sizes: ["xsmall", "small", "medium", "large", "xlarge"],
         }
     },
-
-
-
-methods:
- {
-    addToCart() {
-        this.cart += 1;
-    },
-    removeFromCart() {
-        if(this.cart >= 0)
-        this.cart -= 1;
-    },
-    updateVariant(index) {
-        this.selectedVariant = index;
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
+    methods:{
+        addToCart(){
+            this.cart +=1;
         },
-        image() {
-            return this.variants[this.selectedVariant].image
+        updateVariant(index){
+            this.selectedVariant = index;
+          
         },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        sale() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' is on sale.'
+        removeFromCart(){
+            if (this.cart>0){
+                this.cart -=1;
+            } else{
+                this.cart = 0;
             }
-            return ''
+           
+        },
+    }, 
+    computed:{
+        title(){
+            return this.brand + " " + this.product;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity;
+        },
+        image(){
+            return this.variants[this.selectedVariant].image;
         }
-}
- }
+      
+    }
 })
